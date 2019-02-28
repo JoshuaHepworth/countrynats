d026ab14fb61622e93f72c8e8b7d5fa618c67c66
Filip Tatarnik <ftatarnik@gmail.com>
2019-02-28 09:29:54 -0600

    restructuring app.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBarComponent />
-        <h1>Hey this is a lyft clone where i'm going to practice parallax</h1>
-        <TestComponent />
-        ----------------
-        <TestComponentTwo />
+        <div>
+          <div>
+              <div className="topPart">
+                  <header className="navBar">
+                    <NavBarComponent />
+                  </header>
+                  <section>
+                    <h1>Hey this is a lyft clone where i'm going to practice parallax</h1>
+                    <TestComponent />
+                    ----------------
+                    <TestComponentTwo />
+                  </section>
+              </div>
+              <div className="bottomPart">
+                <footer>
+                </footer>
+              </div>
+            </div>
+        </div>
       </div>
 
     );
