eb979efea6a1472a6d15bc7121a03abe46491689
Filip Tatarnik <ftatarnik@gmail.com>
2019-02-28 09:12:55 -0600

    starting to css the navbar, then going to try to add the parallax shadow effect that the lyft website has

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import TestComponent from './TestComponent';
 import TestComponentTwo from './TestComponentTwo';
+import NavBarComponent from './NavBarComponent';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBarComponent />
         <h1>Hey this is a lyft clone where i'm going to practice parallax</h1>
         <TestComponent />
         ----------------
---
+++ src/NavBarComponent/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+
+class NavBarComponent extends Component {
+    render(){
+        return(
+        	<ul className="navBarComponent">
+        		<li></li>
+        		<li>Home</li>
+        		<li>About</li>
+        		<li>PlaceHolder1</li>
+        		<li>Contact</li>
+        	</ul>
+        )
+    }
+}
+export default NavBarComponent;
