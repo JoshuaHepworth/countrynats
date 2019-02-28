8a908e2d136ea0c620dd8f775ffcf09cb4cff711
Filip Tatarnik <ftatarnik@gmail.com>
2019-02-28 11:25:09 -0600

    breaking down css to just  a 1px black border to try and troubleshoot

--- src/App.js
+++ src/App.js
@@ -8,30 +8,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>
               <div className="topPart">
-                  <header >
                     <NavBarComponent />
-                  </header>
               </div>
-
               <div className="bottomPart">
-                  <section>
                     <h1>Hey this is a lyft clone where i'm going to practice parallax</h1>
                     <TestComponent />
                     ----------------
                     <TestComponentTwo />
-                  </section>
-
-                <section>
-                  <footer>
-                  </footer>
-                </section>
               </div>
             
             </div>
-        </div>
-
     );
   }
 }
