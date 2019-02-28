e0584dcc65853ca0a065d341a450b5aa96de69f3
Filip Tatarnik <ftatarnik@gmail.com>
2019-02-28 10:16:33 -0600

    css troubles

--- src/App.js
+++ src/App.js
@@ -9,25 +9,28 @@ class App extends Component {
     return (
       <div className="App">
         <div>
-          <div>
               <div className="topPart">
-                  <header className="navBar">
+                  <header >
                     <NavBarComponent />
                   </header>
+              </div>
+
+              <div className="bottomPart">
                   <section>
                     <h1>Hey this is a lyft clone where i'm going to practice parallax</h1>
                     <TestComponent />
                     ----------------
                     <TestComponentTwo />
                   </section>
+
+                <section>
+                  <footer>
+                  </footer>
+                </section>
               </div>
-              <div className="bottomPart">
-                <footer>
-                </footer>
-              </div>
+            
             </div>
         </div>
-      </div>
 
     );
   }
