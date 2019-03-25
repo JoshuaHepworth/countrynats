08dec3450a11a765c4946521190f62f6a30e3af0
Josh Hepworth <jrhepworth12@gmail.com>
2019-03-25 17:43:36 -0500

    added photos from the shop

--- src/ImageContainer/index.js
+++ src/ImageContainer/index.js
@@ -24,24 +24,24 @@ function AutoGrid(props) {
     <h1 className="explore"> From the shop </h1>
       <Grid container spacing={10}>
         <Grid item xs={3}>
-          <img className="img" src="https://i.imgur.com/rMjqDpt.jpg"/>
+          <img className="img" src="https://i.imgur.com/GtzPbKm.jpg"/>
         </Grid>
         <Grid item xs={3}>
-          <img className="img" src="https://i.imgur.com/rMjqDpt.jpg"/>
+          <img className="img" src="https://i.imgur.com/E3Gq7q4.jpg"/>
         </Grid>
         <Grid item xs={3}>
-          <img className="img" src="https://i.imgur.com/rMjqDpt.jpg"/>
+          <img className="img" src="https://i.imgur.com/1JIHt7n.jpg"/>
         </Grid>
       </Grid>
       <Grid container spacing={10}>
         <Grid item xs={3}>
-          <img className="img" src="https://i.imgur.com/rMjqDpt.jpg"/>
+          <img className="img" src="https://i.imgur.com/8J3TKFn.jpg"/>
         </Grid>
         <Grid item xs={3}>
-          <img className="img" src="https://i.imgur.com/rMjqDpt.jpg"/>
+          <img className="img" src="https://i.imgur.com/I5xWjAb.jpg"/>
         </Grid>
         <Grid item xs={3}>
-          <img className="img" src="https://i.imgur.com/rMjqDpt.jpg"/>
+          <img className="img" src="https://i.imgur.com/CnFih1D.jpg"/>
         </Grid>
       </Grid>
     </div>
