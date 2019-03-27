cf42632a4e2a80503dc65fbf4d5d935e47e0a0d7
Josh Hepworth <jrhepworth12@gmail.com>
2019-03-27 16:49:19 -0500

    changing image grid for mobile responsive

--- src/ImageContainer/index.js
+++ src/ImageContainer/index.js
@@ -1,56 +1,38 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Divider from '@material-ui/core/Divider';
-import './styles.css';
+import React from "react";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 
-const styles = theme => ({
-  root: {
-    flexGrow: 2,
-  },
-  paper: {
-    padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-});
-
-function AutoGrid(props) {
-  const { classes } = props;
-
-  return (
-    <div className={classes.root}>
-    <h1 className="explore"> From the shop </h1>
-      <Grid container spacing={10}>
-        <Grid item xs={3}>
-          <img className="img" src="https://i.imgur.com/GtzPbKm.jpg"/>
-        </Grid>
-        <Grid item xs={3}>
-          <img className="img" src="https://i.imgur.com/E3Gq7q4.jpg"/>
-        </Grid>
-        <Grid item xs={3}>
-          <img className="img" src="https://i.imgur.com/1JIHt7n.jpg"/>
-        </Grid>
-      </Grid>
-      <Grid container spacing={10}>
-        <Grid item xs={3}>
-          <img className="img" src="https://i.imgur.com/8J3TKFn.jpg"/>
-        </Grid>
-        <Grid item xs={3}>
-          <img className="img" src="https://i.imgur.com/I5xWjAb.jpg"/>
-        </Grid>
-        <Grid item xs={3}>
-          <img className="img" src="https://i.imgur.com/CnFih1D.jpg"/>
-        </Grid>
-      </Grid>
-    </div>
-  );
+class MasksPage extends React.Component {
+  render() {
+    return (
+      <MDBContainer className="mt-5">
+        <MDBRow>
+          <MDBCol lg="12" md="12" className="mb-4">
+            <img src="https://i.imgur.com/GtzPbKm.jpg" className="img-fluid z-depth-1" alt="" />
+          </MDBCol>
+          <MDBCol lg="12" md="6" className="mb-4">
+            <img src="https://i.imgur.com/E3Gq7q4.jpg" className="img-fluid z-depth-1-half"
+              alt="" />
+          </MDBCol>
+          </MDBRow>
+        <MDBRow>
+          <MDBCol lg="12" md="6" className="mb-4">
+            <img src="https://i.imgur.com/1JIHt7n.jpg" className="img-fluid z-depth-2" alt="" />
+          </MDBCol>
+          <MDBCol lg="12" md="12" className="mb-4">
+            <img src="https://i.imgur.com/8J3TKFn.jpg" className="img-fluid z-depth-3" alt="" />
+          </MDBCol>
+        </MDBRow>
+        <MDBRow>
+          <MDBCol lg="12" md="6" className="mb-4">
+            <img src="https://i.imgur.com/I5xWjAb.jpg" className="img-fluid z-depth-4" alt="" />
+          </MDBCol>
+          <MDBCol lg="12" md="6" className="mb-4">
+            <img src="https://i.imgur.com/CnFih1D.jpg" className="img-fluid z-depth-5" alt="" />
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    );
+  }
 }
 
-AutoGrid.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(AutoGrid);
+export default MasksPage;
