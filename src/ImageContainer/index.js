968ad8bea3ae2dee05960ff8632259a97560036b
Josh Hepworth <jrhepworth12@gmail.com>
2019-03-29 12:50:59 -0500

    fixed spacing on images

--- src/ImageContainer/index.js
+++ src/ImageContainer/index.js
@@ -7,27 +7,27 @@ class MasksPage extends React.Component {
     return (
       <MDBContainer className="mt-5">
         <MDBRow>
-          <MDBCol lg="12" md="12" className="ml-3">
+          <MDBCol lg="11" md="12" className="ml-3">
             <img src="https://i.imgur.com/GtzPbKm.jpg" className="img-fluid z-depth-1" alt="" />
           </MDBCol>
-          <MDBCol lg="12" md="6" className="mb-4">
+          <MDBCol lg="11" md="6" className="mb-4">
             <img src="https://i.imgur.com/E3Gq7q4.jpg" className="img-fluid z-depth-1-half"
               alt="" />
           </MDBCol>
           </MDBRow>
         <MDBRow>
-          <MDBCol lg="12" md="6" className="mb-4">
+          <MDBCol lg="11" md="6" className="mb-4">
             <img src="https://i.imgur.com/1JIHt7n.jpg" className="img-fluid z-depth-2" alt="" />
           </MDBCol>
-          <MDBCol lg="12" md="12" className="mb-4">
+          <MDBCol lg="11" md="12" className="mb-4">
             <img src="https://i.imgur.com/8J3TKFn.jpg" className="img-fluid z-depth-3" alt="" />
           </MDBCol>
         </MDBRow>
         <MDBRow>
-          <MDBCol lg="12" md="6" className="mb-4">
+          <MDBCol lg="11" md="6" className="mb-4">
             <img src="https://i.imgur.com/I5xWjAb.jpg" className="img-fluid z-depth-4" alt="" />
           </MDBCol>
-          <MDBCol lg="12" md="6" className="mb-4">
+          <MDBCol lg="11" md="6" className="mb-4">
             <img src="https://i.imgur.com/CnFih1D.jpg" className="img-fluid z-depth-5" alt="" />
           </MDBCol>
         </MDBRow>
