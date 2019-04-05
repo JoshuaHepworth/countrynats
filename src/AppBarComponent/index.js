b691fb7666da6a0d92152e10c7b60f17450323f2
Josh Hepworth <jrhepworth12@gmail.com>
2019-04-05 14:55:16 -0500

    changed top nav to bootstrap

--- src/AppBarComponent/index.js
+++ src/AppBarComponent/index.js
@@ -1,52 +1,47 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import { Link } from 'react-router-dom'
-import './styles.css'
+import React, { Component } from "react";
+import {
+MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBFormInline,
+MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem
+} from "mdbreact";
 
-const styles = {
-  root: {
-    flexGrow: 1,
-    backgroundColor: "blue500",
-  },
-  grow: {
-    flexGrow: 1,
-    marginLeft: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 40,
-  },
+class NavbarPage extends Component {
+state = {
+  isOpen: false
 };
 
-function ButtonAppBar(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar style={{ backgroundColor: 'black' }} color="default" position="static">
-        <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-          </IconButton>
-          <Typography style={{ color: 'white' }} variant="h6" color="default" className={classes.grow}>
-            <strong>316 Campbell Street, Geneva, IL</strong>
-          </Typography>
-          <Typography style={{ color: 'white' }} variant="h6" color="default" className={classes.menuButton}>
-            <strong>(630) 232-1172</strong>
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+toggleCollapse = () => {
+  this.setState({ isOpen: !this.state.isOpen });
 }
 
-ButtonAppBar.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+render() {
+  return (
+      <MDBNavbar color="black" dark expand="md">
+        <MDBNavbarBrand>
+          <strong className="white-text">316 Campbell Street, Geneva, IL</strong>
+        </MDBNavbarBrand>
+        <MDBNavbarToggler onClick={this.toggleCollapse} />
+        <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+          <MDBNavbarNav left>
+            <MDBNavItem active>
+            </MDBNavItem>
+            <MDBNavItem>
+            </MDBNavItem>
+          </MDBNavbarNav>
+          <MDBNavbarNav right>
+            <MDBNavItem>
+              <MDBFormInline waves>
+                <div className="md-form my-0">
+                  <MDBNavbarBrand>
+          <strong className="white-text">(630) 232-1172</strong>
+        </MDBNavbarBrand>
+                </div>
+              </MDBFormInline>
+            </MDBNavItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBNavbar>
+    );
+  }
+}
 
-export default withStyles(styles)(ButtonAppBar);
+export default NavbarPage;
